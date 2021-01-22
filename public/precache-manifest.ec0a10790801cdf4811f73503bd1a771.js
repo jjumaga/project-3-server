a4a556ced1d5dadd1f0cb3ecbebbfeed5dddb269
Jacqueline <j.jumaga@gmail.com>
2021-01-22 14:02:00 +0100

    first deploy

---
+++ public/precache-manifest.ec0a10790801cdf4811f73503bd1a771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e707fe74d24e42f52b2876506448576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11cfbcd4d4333e0e56d9",
+    "url": "/static/css/main.6b3ecbf0.chunk.css"
+  },
+  {
+    "revision": "5d75cf7b7e8d4ed7bdae",
+    "url": "/static/js/2.4871060c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4871060c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cfbcd4d4333e0e56d9",
+    "url": "/static/js/main.cedeb4c6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "08842be961d06ce30408e57c596adec6",
+    "url": "/static/media/homepage-banner.08842be9.png"
+  }
+]);

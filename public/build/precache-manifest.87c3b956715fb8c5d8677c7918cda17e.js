ab04edc4f0eb99295836ebd69f7d0951cdbfbccb
Jacqueline <j.jumaga@gmail.com>
2021-10-01 17:07:27 +0200

    configure environment variable on heroku

---
+++ public/build/precache-manifest.87c3b956715fb8c5d8677c7918cda17e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c4003ce661afe43afdd00b094d25b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5670fb70de47d84086ea",
+    "url": "/static/css/main.d722b0b4.chunk.css"
+  },
+  {
+    "revision": "af803157f528f58dac2f",
+    "url": "/static/js/2.380bfc3a.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/static/js/2.380bfc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5670fb70de47d84086ea",
+    "url": "/static/js/main.549abaa3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9e78791a5084c5a5a6cfcef6ddc8b0c2",
+    "url": "/static/media/banner.9e78791a.jpg"
+  }
+]);

e5677eaa15ad36007c7f00cc36ff0b3235268c27
Jacqueline <j.jumaga@gmail.com>
2021-10-06 16:54:06 +0200

    css theme update

---
+++ build/precache-manifest.ac174c0d17a6ba3df07a0b12c4407044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d540ada5d6346aed4803645e3650f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb6dd8322b339f41e29",
+    "url": "/static/css/main.5c283299.chunk.css"
+  },
+  {
+    "revision": "af803157f528f58dac2f",
+    "url": "/static/js/2.380bfc3a.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/static/js/2.380bfc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb6dd8322b339f41e29",
+    "url": "/static/js/main.9303cba9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b0d5b5671c30395c8861ad1ceff6ec0e",
+    "url": "/static/media/diamondBanner.b0d5b567.png"
+  }
+]);
